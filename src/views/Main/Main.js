1c53cd8f5b2a52cedd5b6027af1e39ad976e01b3
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-15 11:29:25 -0800

    added styling to films

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -3,8 +3,12 @@ import './Main.css';
 export default function Main() {
   return (
     <div className="main">
-      One Ring to rule them all, One Ring to find them, One Ring to bring them all, and in the
-      darkness bind them
+      <div className="mainText">
+        <p>One Ring to rule them all,</p>
+        <p>One Ring to find them,</p>
+        <p>One Ring to bring them all,</p>
+        <p>and in the darkness bind them."</p>
+      </div>
     </div>
   );
 }
