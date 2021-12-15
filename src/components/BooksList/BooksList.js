1797b081e1bacf482cb609f9df4029daf6e34b68
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-12-15 11:58:14 -0800

    added styling to books

--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,5 +1,12 @@
 import './BooksList.css';
 
 export default function BooksList({ title }) {
-  return <div>title: {title}</div>;
+  return (
+    <div className="bookCard">
+      <h1>{title}</h1>
+      <img
+        src={`${process.env.PUBLIC_URL}/FILMS/${title.toLowerCase().replace(/\s/g, '-')}.jpeg`}
+      />
+    </div>
+  );
 }
--- src/components/CharactersList/CharactersList.js
+++ src/components/CharactersList/CharactersList.js
@@ -1,9 +1,5 @@
 import './CharactersList.css';
 
 export default function CharactersList({ name }) {
-  return (
-    <div>
-      <h1 className="characterName">{name}</h1>
-    </div>
-  );
+  return <h1 className="characterName">{name}</h1>;
 }
